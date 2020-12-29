1293721bc755a5bae8eba1a5a272e0f425d4dc33
Paweł Zielonka <lite.what@gmail.com>
2020-12-29 19:47:30 +0100

    Removed comments and moved matchers initalization to main process.

--- main.py
+++ main.py
@@ -29,6 +29,8 @@ from openpyxl import load_workbook
 
 import multiprocessing as mp
 
+import metrics as m
+
 
 def load_nlp_model(name: str):
     """
@@ -67,18 +69,10 @@ def prepare_input_phrases_with_lemmas(nlp_model, filepath: str):
 
     Returns: Pandas dataframe
     """
-    lemmatizer_start = time.time()
-
     phrase_database = pd.read_excel(filepath, engine="openpyxl")
     phrase_database['Lemma'] = phrase_database.apply(partial(lemmatizer, nlp_model), axis=1)
     phrase_database = phrase_database.drop_duplicates(subset=['URL', 'Lemma'])
-    phrase_database.to_excel("Lemmas.xlsx")
-
-    print("Stworzono df z frazami lemma.")
-
-    lemmatizer_end = time.time()
-    lemmatizer_time = lemmatizer_end - lemmatizer_start
-
+    phrase_database.to_excel("Tmp/Lemmas.xlsx")
     return phrase_database
 
 
@@ -165,136 +159,88 @@ def append_df_to_excel(filename, df, sheet_name='Sheet1', startrow=None,
     # save the workbook
     writer.save()
 
+
 # TODO: The function name is bad, because it doesn't tell too much about what it's doing FIXME.
-
-
-def process_url(nlp_model, database_df, destination_url_list, input_class, url):
-    tokenization_start = time.time()
-
-    # print(f'URL {url_idx} z {urls_count}')
-    # print(f'Zapisano {counter} wierszy w raporcie.')
-
-    # http_start = time.time()
+def process_url(nlp_model, matchers, destination_url_list, input_class, url):
+    # print("Processing url:", url)
     url_info = get_information_from_soup(url=url, input_class=input_class)
-    # http_stop = time.time()
-    # http_delta = http_stop - http_start
-    # print(f"HTTP DELTA: {http_delta}s")
     text = url_info['Tekst']
-
     document = nlp_model(text)
 
-    # tokenization_stop = time.time()
-    # tokenization_delta = tokenization_stop - tokenization_start
-    # print("Tokenization delta:", tokenization_delta)
-    # tokenization_time += tokenization_delta
-
     for d_url in destination_url_list:
-        # TODO: Context of measuring finding a match
-        # finding_match_time_start = time.time()
-
+        # print("Checking url:", d_url)
         if url == d_url:
+            print(">>>>>>>>>> Self matched!")
             continue  # przerwanie pętli, żeby nie szukać dopasowań na siebie.
 
-        # zrzutowanie fraz przyporządkowanych do danego URLa na urla
-        # tworzenie patternów na podstawie silnika nlp
-        phrase_list = list(database_df['Lemma'][database_df['URL'] == d_url])
-        phrase_patterns = [nlp_model(text) for text in phrase_list]
-
-        # zainicjowanie pustego matchera z atrubutem lemma - szuka podstawowych form dla każdego tokenu
-        matcher = PhraseMatcher(nlp_model.vocab, attr="LEMMA")
-        # TODO: A może matcher powinien mieć id = d_url i nie musimy go inicjalizować za każdym razem w pętli?
-        matcher.add('Szukacz', None, *phrase_patterns)
-
-        # tworzymy obiekt ze znalezionymi (lub nie) frazami w tekście
+        matcher = matchers[d_url]
         found_matches = matcher(document)
-        # finding_match_time_stop = time.time()
-        # finding_match_time += finding_match_time_stop - finding_match_time_start
-        # TODO: Close context of measuring finding a match
-
-        # TODO: Context of measuring writing to excel (writing what?)
-        # writing_to_excel_start = time.time()
 
         if len(found_matches) == 0:
+            # print("Not found!")
             pass
         else:
-            # TODO: Looks like match_id is not used anywhere
             rows = []
             for match_id, start, end in found_matches:  # tuple unpacking - potrzebujemy tylko start oraz end
-                # TODO: lock! Removed counter for now to simplify the process
-
                 phrase = document[start:end]  # fraza pokrewna znaleziona w tekście
                 span = document[start - 5:end + 6]  # tworzenie kontekstu dla znalezionej frazy
-
-                # TODO: Probably the result of incoming process
                 list_row = [url, d_url, phrase.text, span.text]
                 rows.append(list_row)
-
-                # TODO: LOCK or send to synchronized queue
                 # append_list_to_excel(filename='Output/Raport linkowania.xlsx',
                 #                      list_name=list_row,
                 #                      sheet_name='Raport')
-
-        # writing_to_excel_stop = time.time()
-        # writing_to_excel_time += writing_to_excel_stop - writing_to_excel_start
-        # TODO: End context of measuring writing to excel (writing what?)
+            print(f">>>>>>>>>> Found {len(found_matches)} matches!")
+    # print("Done")
 
 
-
-# append_list_to_excel(filename='Output/Raport linkowania.xlsx',
-#                      list_name=list_row,
-#                      sheet_name='Raport')
+def init_matchers(nlp_model, database_df, destination_urls):
+    d = dict()
+    mtr = m.Metrics('matcher')
+    for url in destination_urls:
+        mtr.start()
+        phrase_list = list(database_df['Lemma'][database_df['URL'] == url])
+        phrase_patterns = [nlp_model(text) for text in phrase_list]
+        matcher = PhraseMatcher(nlp_model.vocab, attr="LEMMA")
+        matcher.add(url, None, *phrase_patterns)
+        d[url] = matcher
+        mtr.stop()
+    mtr.report()
+    return d
 
 
 def create_inlinks_report(nlp_model, source_url_list, database_df, input_class):
-    # counter = 0
-
-    # wyodrębnienie wszystkich docelowych URLi
-    destination_url_list = database_df['URL'].unique()
-
-    # TODO: Determine shared state between processes
-    # tokenization_time = 0
-    # finding_match_time = 0
-    # writing_to_excel_time = 0
-
-    target = partial(
-        process_url,
-        nlp_model, database_df, destination_url_list, input_class
-    )
-
-    proc_num = mp.cpu_count() - 1
-
-    # results = list(map(target, source_url_list))
-
     mp.set_start_method('spawn', True)
 
-    with mp.Pool(8) as p:
+    destination_url_list = database_df['URL'].unique()
+
+    print("Initializing matchers")
+    matchers = init_matchers(nlp_model, database_df, destination_url_list)
+    print("Matchers initialized")
+
+    target = partial(process_url, nlp_model, matchers, destination_url_list, input_class)
+
+    # proc_num = mp.cpu_count() - 1
+
+    with mp.Pool(10) as p:
         results = p.map(target, source_url_list)
 
-    print(f'Processed {len(results)} results')
-
     return results
-    # return tokenization_time, finding_match_time, writing_to_excel_time
 
 
 if __name__ == '__main__':
-    overall_start = time.time()
+    mo = m.Metrics('overall')
+    mo.start()
 
     lang_model = load_nlp_model("pl_core_news_sm")
-    url_list = load_url_list("Input/url_list2.txt")
-    df_phrases = prepare_input_phrases_with_lemmas(lang_model, "Input/asd.xlsx")
-    prepare_output_sheet('Output/Raport linkowania.xlsx')
+    url_list = load_url_list("Input1/url_list.txt")
+    df_phrases = prepare_input_phrases_with_lemmas(lang_model, "Input1/Morele_ahrefs.xlsx")
+    prepare_output_sheet('Output/report.xlsx')
 
-    # tokenization_time, finding_match_time, writing_to_excel_time = \
-    #     create_inlinks_report(queue, lang_model, url_list, df_phrases, input_class='entry-content')
+    create_inlinks_report(lang_model, url_list, df_phrases, input_class='single-news-container')
 
-    create_inlinks_report(lang_model, url_list, df_phrases, input_class='entry-content')
+    mo.stop()
+    mo.report()
 
-    print("Finished multiprocessing!")
-
-    overall_stop = time.time()
-    overall_time = overall_stop - overall_start
-
-    print(f"Całkowity czas działania: {overall_time} s.")
 
 
 # TODO: Collect metrics and report them
--- metrics.py
+++ metrics.py
@@ -1,21 +1,24 @@
+import numpy as np
 import time
 from collections import namedtuple
-
-# metrics = Metrics('processing_url')
-# metrics.start()
-# metrics.stop()
-# metrics.report()
-# metrics.reset()
 
 
 Measurement = namedtuple('Measurement', ['start_time', 'end_time'])
 
 
-# statuses:
-# - idle
-# - pending
-
 class Metrics:
+    # metrics = Metrics('processing_url')
+    # metrics.start()
+    # metrics.stop()
+    # metrics.report()
+    # metrics.reset()
+    # metrics.durations()
+    # metrics.total_duration()
+
+    # statuses:
+    # - idle
+    # - pending
+
     def __init__(self, name):
         super().__init__()
         self.name = name
@@ -35,9 +38,38 @@ class Metrics:
         self.status = 'idle'
 
     def report(self):
-        raise NotImplementedError()
+        print(f"*************** {self.name} ****************")
+        print("Total duration:", self.total_duration())
+        print("Min:", self.min_duration())
+        print("Max:", self.max_duration())
+        print("Mean:", self.mean())
+        print("Std dev:", self.std_dev())
+        print("***********************************")
 
     def reset(self):
         self.measurements = list()
         self.start_time = None
         self.status = 'idle'
+
+    def durations(self):
+        return map(lambda x: x.end_time - x.start_time, self.measurements)
+
+    def max_duration(self):
+        arr = np.array(list(self.durations()))
+        return arr.max()
+
+    def min_duration(self):
+        arr = np.array(list(self.durations()))
+        return arr.min()
+
+    def total_duration(self):
+        arr = np.array(list(self.durations()))
+        return arr.sum()
+
+    def mean(self):
+        arr = np.array(list(self.durations()))
+        return arr.mean()
+
+    def std_dev(self):
+        arr = np.array(list(self.durations()))
+        return arr.std()
